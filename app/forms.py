7bb673d385280c6a4214d2360d33294921e84a17
bembite <volodya.ternopil1997@gmail.com>
2017-11-23 00:47:37 +0200

    start creating forms

---
+++ app/forms.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*- 
+from flask_wtf import FlaskForm
+from wtforms import validators
+from wtforms.fields import TextField, TextAreaField, SubmitField
+from wtforms.fields.html5 import EmailField
+
+class ContactForm(FlaskForm):
+	 name = TextField("name")
+	 #email = TextField('Email address', [validators.DataRequired(), validators.Email("kek")])
+	 
+	 email = EmailField("Email",[validators.DataRequired("Please enter your email address."),validators.Email("Please enter your email address.")])
+	 phone=TextField("Phone")
+	 time=TextField("Time")
+	 message = TextAreaField("Message")
+	 submit = SubmitField("Send")
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,12 @@
+# -*- coding: utf-8 -*- 
 from flask import Flask, render_template, request, flash, json
+from forms import ContactForm
 
 
 
 application = Flask(__name__)
 
 application.secret_key = 'development key'
-
 #mail.init_app(app)
 @application.route('/')
 def home():
@@ -14,6 +15,17 @@ def home():
 @application.route('/kek')
 def maitanance():
   return render_template('maitanance.html')
+
+@application.route('/contact', methods=['GET', 'POST'])
+def contact():
+  form = ContactForm()
+ 
+  if request.method == 'POST':
+    return 'Form posted.'
+ 
+  elif request.method == 'GET':
+    return render_template('contact.html', form=form)
+
   
 if __name__ == '__main__':
   #application.run(debug=True)
